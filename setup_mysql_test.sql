6d5a0a98d3238a17fb25d6f8c2cc9f3107d60c27
Sirius1616 <siriusa1.615@gmail.com>
2025-10-01 19:16:48 +0100

    test file update

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -2,8 +2,8 @@
 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 
-CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED WITH hbnb_test_pwd;
+CREATE USER IF NOT EXISTS hbnb_test@localhost IDENTIFIED BY 'hbnb_test_pwd';
 
-GRANT ALL PRIVILEGES ON hbnb_test_db to hbnb_test@localhost WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO hbnb_test@localhost WITH GRANT OPTION;
 
-GRANT SELECT FOR hbnb_test ON performance_schema.*;
+GRANT SELECT ON performance_schema.* TO hbnb_test@localhost;
