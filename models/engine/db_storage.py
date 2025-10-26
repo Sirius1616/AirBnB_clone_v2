fe4bf4d5371f7a74a055f5fcd90c87669476d33e
Sirius1616 <siriusa1.615@gmail.com>
2025-10-27 00:34:01 +0100

    Database storage implementation

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python
+from sqlalchemy import create_engine
+from os import getenv
 
+user = getenv('HBNB_MYSQL_USER')
+password = getenv('HBNB_MYSQL_PWD')
+host = getenv('HBNB_MYSQL_HOST')
+database = getenv('HBNB_MYSQL_DB')
 
 class DBStorage:
     """New engine for the model, that implements interaction with the database"""
 
+    __engine = None
+    __session = None
+
     def __init__(self):
-        self.__engine = None
-        self.__session = None
+        self.__engine = engine(f'mysql+mysqldb://{user}:{password}@{host}/{database}, pool_pre_ping=True')
+
+    
+
