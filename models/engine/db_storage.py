b84d802bfb2afe7a133ccab944b14b52a7b2c74a
Sirius1616 <siriusa1.615@gmail.com>
2025-10-26 23:49:08 +0100

    Database storage implementation

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -4,6 +4,8 @@
 class DBStorage:
     """New engine for the model, that implements interaction with the database"""
 
-    def __init__(self, engine=None, session=None):
-        self.__engine = engine
-        self.__session = session
+    def __init__(self):
+        self.__engine = None
+        self.__session = None
+
+    
