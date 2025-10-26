5518e3a42ee59cf90b7e7549bc6f47fcf31b9409
Sirius1616 <siriusa1.615@gmail.com>
2025-10-26 21:56:09 +0100

    Updating the state and city class

--- models/city.py
+++ models/city.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from State import state
+from state import State
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey(state.id), nullable=False)
-
-    state = relationship('State', back_populates='cities')
+    state_id = Column(String(60), ForeignKey('state.id'), nullable=False)
+    places = relationship('places', backref='cities', cascade = 'all, delete, delete-orphan')
+    
     
--- models/state.py
+++ models/state.py
@@ -3,13 +3,33 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String,
 from storage import FileStorage
+from os import getenv
 
 
 class State(BaseModel, Base):
     """ State class """
+    t_storage = getenv('HBNB_TYPE_STORAGE')
+
     __tablename__ = 'states
     name = Column(String(128), nullable= False)
+
+    if t_storage == 'DB':
+        cities = relationship('cities', backref='state', cascade = 'all, delete, delete-orphan')
     
-    cities = relationship('cities', back_populates='state', cascade = 'all', delete)
-    
+    else:
+        @property
+        """Return list of city instances with state_id equal to current State"""
+        def cities(self):
+            from models.city import cities
+
+            city_list = []
+            for city in models.storage.all(City).value():
+                if cities.state_id == self.id:
+                    city_list.append(city)
+            return city_list
+
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+
+
     
