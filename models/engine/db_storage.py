4eaa7efcd6b5b218414adcfd264ee036c3bb2e78
Sirius1616 <siriusa1.615@gmail.com>
2025-10-27 23:41:28 +0100

    Database storage implementation

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -11,7 +11,7 @@ class DBStorage:
 
     def __init__(self):
         user = getenv('HBNB_MYSQL_USER')
-        password = getenv('HBNB_MYSQL_PWD')
+        password = getenv('HBNB_MYSQL_PWD'
         host = getenv('HBNB_MYSQL_HOST')
         database = getenv('HBNB_MYSQL_DB')
         self.__engine = engine(f'mysql+mysqldb://{user}:{password}@{host}/{database}, pool_pre_ping=True')
