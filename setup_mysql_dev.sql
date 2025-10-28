da38787ddb6d5e59de43a36e1f37cb1eab6aaf75
Sirius1616 <siriusa1.615@gmail.com>
2025-10-28 23:25:17 +0100

    Database storage implementation

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -6,7 +6,7 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'Siriusa1.615';
 
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'root'@'localhost' WITH GRANT OPTION;
 
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost'
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO hbnb_dev@localhost;
 
