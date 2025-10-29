3b873802c3f9b5fcb824057a4a17049317bbbb91
Sirius1616 <siriusa1.615@gmail.com>
2025-10-29 23:10:45 +0100

    Database storage implementation

--- models/city.py
+++ models/city.py
@@ -2,7 +2,7 @@
 """ City Module for HBNB project """
 from state import State
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String, ForeignKey, Integer
 from sqlalchemy.orm import relationship
 from os import getenv
 
@@ -11,7 +11,8 @@ class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey('state.id'), nullable=False)
+    state_id = Column(Integer, ForeignKey('state.id'), nullable=False)
     places = relationship('places', backref='cities', cascade = 'all, delete, delete-orphan')
+    
     
     
