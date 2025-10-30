f7063638b5b21e50dd62b7f70723e41145977516
Sirius1616 <siriusa1.615@gmail.com>
2025-10-30 23:26:30 +0100

    Database storage implementation

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -16,7 +16,9 @@ class DBStorage:
         database = getenv('HBNB_MYSQL_DB')
         self.__engine = engine(f'mysql+mysqldb://{user}:{password}@{host}/{database}, pool_pre_ping=True')
 
-    if os.getenv('HBNB_ENV') = 'test':
-        Base.metadata.drop_all(self.__engine)
+        if os.getenv('HBNB_ENV') = 'test':
+            Base.metadata.drop_all(self.__engine)
+
+    
 
 
