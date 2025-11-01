628f1cf17b20b73526f7168f81421dca81661f7f
Sirius1616 <siriusa1.615@gmail.com>
2025-11-01 22:50:38 +0100

    Database storage implementation

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
-
+from models.DBStorage import DBStorage
 
 storage = FileStorage()
 storage.reload()
