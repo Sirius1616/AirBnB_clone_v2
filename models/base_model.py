b8d310d83bd127b906cf0be613a768cc720f1718
Sirius1616 <siriusa1.615@gmail.com>
2025-11-03 23:44:16 +0100

    Update error

--- models/base_model.py
+++ models/base_model.py
@@ -23,7 +23,7 @@ class BaseModel:
             from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow(
+            self.updated_at = datetime.utcnow()
         else:
             for key, value in kwargs.items():
                 if key == '__class__':
