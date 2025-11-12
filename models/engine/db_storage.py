91f8b602d719734354df39317274985a1372ce66
Sirius1616 <siriusa1.615@gmail.com>
2025-11-13 00:48:10 +0100

    Updated

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -29,6 +29,7 @@ class DBStorage:
         from models.place import Place
         from models.state import State
         from models.city import City
+        from models.amenity import Amenity
         from models.review import Review
 
         classes = {
