ee167ff2ed18cad309dc1aa0aef004d205252d24
Sirius1616 <siriusa1.615@gmail.com>
2025-11-12 22:21:25 +0100

    Updated

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,15 @@
 """This module instantiates an object of class FileStorage"""
 from os import getenv
 
+#import all models so sqlalchemy can discover them
+from models.base_model import BaseModel, Base
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+
 
 storage_type = getenv('HBNB_TYPE_STORAGE')
 
